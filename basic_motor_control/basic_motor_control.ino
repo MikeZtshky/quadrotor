3b1c942649431ef58f35f9a52695bd25ae0e6aa8
Hayk Martirosyan <hayk.mart@gmail.com>
2013-09-29 20:58:17 -0700

    modularized, command parser, motor driver

--- basic_motor_control/basic_motor_control.ino
+++ basic_motor_control/basic_motor_control.ino
@@ -1,83 +1,58 @@
 
+/****************************************************
+* Basic Quadrotor Motor Control
+* Hayk Martirosyan
+****************************************************/
 
-#include <Servo.h> 
+
 #include "Timer.h"
+#include "Servo.h"
+
+#include "constants.h"
+#include "command_parser.h"
+#include "motor_driver.h"
 
 // --------------------------------------------------
 // CONSTANTS
 // --------------------------------------------------
 
-#define SERIAL_BUFFER_SIZE 15
-
 // --------------------------------------------------
 // VARIABLES
 // --------------------------------------------------
 
-// One servo object per motor stack
-Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo servo4;
-
 Timer t;
-
-double pos;
-double goal;
-
-String serial_received = String();
 
 // --------------------------------------------------
 // FUNCTIONS
 // --------------------------------------------------
 
-void print_pos() {
-  Serial.print("Position: ");
-  Serial.println(servo1.readMicroseconds());
-}
-
-void get_serial() {
-  
-  if (Serial.available()) {
-    
-    int i;
-    for(i = 0; Serial.available(); i++) {
-      serial_received[i] = Serial.read();
-    }
-    serial_received[i] = '\0';
-    
-    Serial.print("Received: ");
-    Serial.println(serial_received);
-  }
-}
-
-//void check_serial_command(char[] command) {
-  //
-//1}
+// --------------------------------------------------
+// MAIN FUNCTIONS
+// --------------------------------------------------
 
 void setup() 
 { 
-  // Attach the servos to pins
-  servo1.attach(11);
-  
-  pos = 1500;
-  goal = 2000;
-  
-  servo1.writeMicroseconds(int(pos));
   
   Serial.begin(9600);
-  t.every(1000, print_pos);
-  t.every(100, get_serial); 
+  
+  CMD_init();
+  MDR_init();
+  
+  // Update each module
+  t.every(CMD_UPDATE_RATE, CMD_update);
+  t.every(MDR_UPDATE_RATE, MDR_update);
+  
+  // Parse each module's commands
+  t.every(CMD_UPDATE_RATE, MDR_commands);
+  
+  t.every(1000, MDR_print_position);
 }
 
-void loop() 
-{ 
+void loop() {
   
+
   
-  if (pos < goal)
-     pos += .1;
-  else if (pos > goal)
-     pos -= .1;
-  
-  servo1.writeMicroseconds(int(pos));
+  //servo1.writeMicroseconds(int(pos));
   t.update();
 } 
+
